c84a2fe2735dacfe4c08543c6b80d068abbc8e33
yeren108 <623486747@qq.com>
2018-03-26 18:17:18 +0800

    TODO 这是手机适配页面，暂时未开发

--- src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/MobileInterceptor.java
+++ src/main/java/com/thinkgem/jeesite/modules/sys/interceptor/MobileInterceptor.java
@@ -32,7 +32,8 @@ public class MobileInterceptor extends BaseService implements HandlerInterceptor
 		if (modelAndView != null){
 			// 如果是手机或平板访问的话，则跳转到手机视图页面。
 			if(UserAgentUtils.isMobileOrTablet(request) && !StringUtils.startsWithIgnoreCase(modelAndView.getViewName(), "redirect:")){
-				modelAndView.setViewName("mobile/" + modelAndView.getViewName());
+				//TODO 这是手机适配页面，暂时未开发
+				//modelAndView.setViewName("mobile/" + modelAndView.getViewName());
 			}
 		}
 	}
